f3077372826a66297e94df235b3bf3b51c81e90f
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-04-24 18:30:06 +0600

    loca storage added

--- src/redux/features.ts/cartSlice.ts
+++ src/redux/features.ts/cartSlice.ts
@@ -1,20 +1,68 @@
 import { TMedicine } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 
-interface InitialState  {
-    medicines: TMedicine[]
+export interface CartProduct extends TMedicine {
+    orderQuantity: number
 }
 
-const initialState: InitialState = {
-    medicines: []
+interface InitialState {
+    medicines: CartProduct[]
+    city: string;
+    shippingAddress: string;
+
 }
+
+// helper function to load cart from local storage
+
+const loadCartFromLocalStorage = (): InitialState => {
+    if (typeof window === 'undefined') {
+        return {
+            medicines: [],
+            city: '',
+            shippingAddress: ''
+        }
+    }
+    try {
+        const storedCart = localStorage.getItem('cart');
+        if (storedCart) {
+            return JSON.parse(storedCart)
+        }
+    } catch (error) {
+        console.log('Failed to load cart from localStorage:', error)
+    }
+    return {
+        medicines: [],
+        city: '',
+        shippingAddress: ''
+    }
+}
+
+const saveCartToLocalStorage = (cart: InitialState)=> {
+    if (typeof window === "undefined") {
+        return
+    }
+    try {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    } catch (error) {
+        console.error('Failed to save cart to localStorage:', error);
+    }
+}
+
+const initialState: InitialState = loadCartFromLocalStorage()
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        
+        addProduct: (state, action) => {
+            console.log('clicked',{state, action});
+            // if (state.medicines.length === 0) {
+            //     state.medicines.
+            // }
+        }
     }
 })
+
+export const {addProduct} = cartSlice.actions;
 
 export default cartSlice.reducer 
