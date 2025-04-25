e438f99e1563bc954590a9b5b8a9a2c8f3025ae9
Noc1 PC MargoN <monondcosta@gmail.com>
2025-04-26 03:52:12 +0600

    Add: added manage orders

--- src/components/modules/dashboard/app-sidebar.tsx
+++ src/components/modules/dashboard/app-sidebar.tsx
@@ -77,17 +77,17 @@ const data = {
       url: "/customer",
       icon: Bot,
       items: [
-        {
-          title: "Manage Products",
-          url: "//medicines",
-        },
-        {
-          title: "Manage Users",
-          url: "/admin/users",
-        },
+        // {
+        //   title: "Manage Products",
+        //   url: "/medicines",
+        // },
+        // {
+        //   title: "Manage Users",
+        //   url: "/admin/users",
+        // },
         {
           title: "Manage Orders",
-          url: "/admin/orders",
+          url: "/customer/orders",
         },
       ],
     },
--- src/components/modules/dashboard/nav-user.tsx
+++ src/components/modules/dashboard/nav-user.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { ChevronsUpDown } from "lucide-react";
