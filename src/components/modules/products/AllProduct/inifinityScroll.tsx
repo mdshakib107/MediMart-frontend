01785a332064fea94f901322734a3184ec646471
MarGoN <monondcosta@gmail.com>
2025-05-03 21:20:49 +0600

    Fix: fixed infinite scrolling contents(responsiveness)

--- src/components/modules/products/AllProduct/inifinityScroll.tsx
+++ src/components/modules/products/AllProduct/inifinityScroll.tsx
@@ -89,7 +89,7 @@ const InfiniteProductList = ({ filters }: { filters: Record<string, any> }) => {
           </p>
         }
       >
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 pt-3">
           {products.map((medicine, index) => (
             <ProductCard key={`${medicine._id}-${index}`} medicine={medicine} />
           ))}
